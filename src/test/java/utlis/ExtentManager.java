f68190c75885652468a3bd084668b165d7804e95
Sneha Mahajan <120576246+sneha056@users.noreply.github.com>
2025-09-24 11:17:00 +0530

    documentation added

--- src/test/java/utlis/ExtentManager.java
+++ src/test/java/utlis/ExtentManager.java
@@ -1,18 +1,22 @@
  package utlis;
 
  /* to create reports if they are not created yet using extent report */
- 
+
+//The main reporting object that manages the test logs and results.
 import com.aventstack.extentreports.ExtentReports;
+
+//Helps in generating a nice HTML report (in your case, at "test-output/ExtentReport.html").
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
-	public static ExtentReports extent;
+	public static ExtentReports extent; //static variable holding a single instance of ExtentReports
 	public static ExtentReports getInstance() {
-		if(extent ==null) {
+		if(extent ==null) { //Checks if the report object is not already created
 			ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/ExtentReport.html");
 			extent= new ExtentReports();
-			extent.attachReporter(reporter);
+			extent.attachReporter(reporter); //Attaches the reporter so that results will be logged into the HTML file.
 		}
-		return extent;
+		return extent; //If the report already exists, just return the existing one.If not, create it, attach reporter, and return it.
+
 	}
 }
